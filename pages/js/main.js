e33ed04869c60a116419a086abb67447887979d0
Егор Черкасов <egor-cherkasoff.ps@yandex.ru>
2023-05-21 18:56:13 +0800

    contact section complete

--- pages/js/main.js
+++ pages/js/main.js
@@ -1,5 +1,7 @@
 import Burger from "./components/burger.js";
 import Slider from "./components/slider.js";
+import Button from "./components/button.js";
 
 const burger = new Burger();
 const slider = new Slider();
+const button = new Button();
